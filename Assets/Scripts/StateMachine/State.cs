e8d1b88776d3895058f4cd56c5b73d339e364632
zipoz <thezipoz89@gmail.com>
2023-01-25 00:11:29 +0100

    Removed old files

--- Assets/Scripts/StateMachine/State.cs
+++
@@ -1,9 +0,0 @@
-namespace Old
-{
-    public abstract class State
-    {
-        public abstract void OnStateEnter();
-        public abstract void OnStateUpdate();
-        public abstract void OnStateExit();
-    }
-}
--- Assets/Scripts/Temp/IPoolableObject.cs
+++
@@ -1,6 +0,0 @@
-public interface IPoolableObject
-{
-    public void OnGenerated(int objectId);
-    public void OnSpawned();
-    public void OnReturned();
-}
